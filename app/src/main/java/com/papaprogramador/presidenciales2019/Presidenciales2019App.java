a9a8676975bcea62e6bf6bf636e446f62cba9e3b
AndersonCastellon <anderson.ivan.fuentes@gmail.com>
2018-03-25 22:50:51 -0600

    SDK de Facebook implementado

---
+++ app/src/main/java/com/papaprogramador/presidenciales2019/Presidenciales2019App.java
@@ -0,0 +1,19 @@
+package com.papaprogramador.presidenciales2019;
+
+import android.app.Application;
+
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by ander on 25/3/2018.
+ */
+
+public class Presidenciales2019App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AppEventsLogger.activateApp(this);
+    }
+}
