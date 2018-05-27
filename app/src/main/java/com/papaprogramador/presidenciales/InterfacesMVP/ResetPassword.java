db5e4a8970731e79da9649ed3cb5a7b57704faba
AndersonCastellon <anderson.ivan.fuentes@gmail.com>
2018-05-26 21:26:05 -0600

    Primera implementacion de patron MVP con Mosby

--- app/src/main/java/com/papaprogramador/presidenciales/InterfacesMVP/ResetPassword.java
+++ app/src/main/java/com/papaprogramador/presidenciales/InterfacesMVP/ResetPassword.java
@@ -1,23 +1,33 @@
 package com.papaprogramador.presidenciales.InterfacesMVP;
 
+import com.hannesdorfmann.mosby3.mvp.MvpPresenter;
+import com.hannesdorfmann.mosby3.mvp.MvpView;
+
 public interface ResetPassword {
 
-	interface Vista{
+	interface Vista extends MvpView {
 		void mostrarResultadoExitoso();
+
 		void errorPorEmailSinCuentaAsociada();
+
 		void errorPorCampoVacio();
+
 		void errorPorEmailInvalido();
 	}
 
-	interface Presentador{
+	interface Presentador extends MvpPresenter<ResetPassword.Vista>{
 		void enviarResultadoExitoso();
+
 		void enviarEmailSinCuentaAsociada();
+
 		void enviarErrorPorCampoVacio();
+
 		void enviarErrorPorEmailInvalido();
+
 		void procesarEmailUsuario(String emailUsuario);
 	}
 
-	interface Modelo{
+	interface Modelo {
 		void resetEmailUsuario(String emailUsuario);
 	}
 }
--- app/src/main/java/com/papaprogramador/presidenciales/Vista/Actividades/ResetPasswordVista.java
+++ app/src/main/java/com/papaprogramador/presidenciales/Vista/Actividades/ResetPasswordVista.java
@@ -1,6 +1,7 @@
 package com.papaprogramador.presidenciales.Vista.Actividades;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.hannesdorfmann.mosby3.mvp.MvpActivity;
 import com.papaprogramador.presidenciales.InterfacesMVP.ResetPassword;
 import com.papaprogramador.presidenciales.Presentador.ResetPasswordPresentador;
 import com.papaprogramador.presidenciales.R;
 
-public class ResetPasswordVista extends AppCompatActivity implements ResetPassword.Vista {
+public class ResetPasswordVista extends MvpActivity<ResetPassword.Vista, ResetPassword.Presentador>
+		implements ResetPassword.Vista {
 
 	private TextInputEditText ResetEmail;
 	private Button btnResetPass;
-	private ResetPassword.Presentador presentador;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +31,20 @@ public class ResetPasswordVista extends AppCompatActivity implements ResetPasswo
 			@Override
 			public void onClick(View v) {
 				String emailUsuario = ResetEmail.getText().toString();
-				presentador.procesarEmailUsuario(emailUsuario);
+				getPresenter().procesarEmailUsuario(emailUsuario);
 			}
 		});
+	}
+
+	@NonNull
+	@Override
+	public ResetPassword.Presentador createPresenter() {
+		return new ResetPasswordPresentador(this);
 	}
 
 	private void onStartVista() {
 		ResetEmail = findViewById(R.id.EmailReset);
 		btnResetPass = findViewById(R.id.btnResetpassword);
-		presentador = new ResetPasswordPresentador(this);
 	}
 
 	@Override
