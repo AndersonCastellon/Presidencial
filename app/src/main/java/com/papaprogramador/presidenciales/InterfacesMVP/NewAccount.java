3568fcd046ca06525d3e6b99fdaf9daefd29ffd2
AndersonCastellon <anderson.ivan.fuentes@gmail.com>
2018-05-27 22:00:58 -0600

    Interfaces para MVP de creacion de cuentas en la app

--- app/src/main/java/com/papaprogramador/presidenciales/InterfacesMVP/NewAccount.java
+++ app/src/main/java/com/papaprogramador/presidenciales/InterfacesMVP/NewAccount.java
@@ -1,18 +1,49 @@
 package com.papaprogramador.presidenciales.InterfacesMVP;
 
+import android.content.Context;
+
+import com.google.firebase.database.DataSnapshot;
 import com.hannesdorfmann.mosby3.mvp.MvpPresenter;
 import com.hannesdorfmann.mosby3.mvp.MvpView;
 
+// Logica
+// 1-Obtener en hilo secundario el ID del dispositivo y almacenarlo en una variable en el presenter
+// 2-Obtener en hilo secundario el ID desde Firebase y almacenarlo en una variable en el presenter
+// 3-Verificar los campos en el modelo y notificar al presenter quien dirá a la vista si no hay error
+// 4-Si no hay errores, enviar los ID´s y el email a validar al modelo
+// 5-Si es true, ir al método para crear la cuenta con los valores que necesite
+// 6-Se registra el usuario en Firebase con los datos obtenidos y se lanza la activity para verificar el email
+
 public interface NewAccount {
 	interface Vista extends MvpView {
-
+		void verificarEmail(String emailUsuario, String password);
+		void errorEnCampo(String error);
+		void errorAlCrearCuenta();
+		void mostrarProgreso();
 	}
 
 	interface Presentador extends MvpPresenter<NewAccount.Vista> {
+		void obtenerIdDispositivo(Context context);
+		void almacenarIdDispositivo(String idDispositivo);
+		void obtenerIdFirebase(String idDispositivo);
+		void almacenarIdFirebase(DataSnapshot idFirebase);
+		void validarCampos(String nombreUsuario, String emailUsuario, String emailUsuario2, String pass,
+		                   String pass2, String departamento);
+		void errorEnCampo(String error);
+		void errorAlCrearCuenta();
+		void irAVerificarEmail(String emailUsuario, String pass);
 
 	}
 
 	interface Modelo {
-
+		void almacenarIdDispositivo(String idDispositivo);
+		void almacenarIdFirebase(DataSnapshot idFirebase);
+		void validarCampos(String nombreUsuario, String emailUsuario, String emailUsuario2, String pass,
+		                   String pass2, String departamento);
+		void validarIdEnFirebase(DataSnapshot idFirebase, String idDispositivo,
+		                         String emailUsuario, String nombreUsuario, String pass, String departamento);
+		void crearCuenta(String emailUsuario, String nombreUsuario, String pass, String departamento);
+		void registrarUsuarioEnFirebase(String nombreUsuario, String emailUsuario,
+		                                String departamento, String idDispositivo, String voto);
 	}
 }
