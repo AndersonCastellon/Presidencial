2e5a1bdc4c541e98421821e2b40908b4b3ce4d3a
AndersonCastellon <anderson.ivan.fuentes@gmail.com>
2018-06-01 23:35:25 -0600

    Iniciando implementacion MVP para la vista del login de la app

--- app/src/main/java/com/papaprogramador/presidenciales/InterfacesMVP/Login.java
+++ app/src/main/java/com/papaprogramador/presidenciales/InterfacesMVP/Login.java
@@ -1,14 +1,20 @@
 package com.papaprogramador.presidenciales.InterfacesMVP;
 
+import android.content.Context;
+
 import com.hannesdorfmann.mosby3.mvp.MvpPresenter;
 import com.hannesdorfmann.mosby3.mvp.MvpView;
 
 public interface Login {
 	interface Vista extends MvpView {
+		void almacenarID(String idDispositivo);
+		void idYaUtilizado();
 
 	}
 
 	interface Presentador extends MvpPresenter<Login.Vista> {
+		void obtenerIdDispositivo(Context context);
+		void obtenerIdFirebase(String idDispositivo);
 
 	}
 
